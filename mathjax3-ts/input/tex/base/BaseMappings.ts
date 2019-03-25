3611f85c3320472940526d9b036a81752a9a5b30
OlgaRedozubova <olgared82@gmail.com>
2019-03-25 09:55:17 +0200

    changed '\u210F' => '\u0127' for \hbar

--- mathjax3-ts/input/tex/base/BaseMappings.ts
+++ mathjax3-ts/input/tex/base/BaseMappings.ts
@@ -112,7 +112,8 @@ new sm.CharacterMap('mathchar0mi', ParseMethods.mathchar0mi, {
   // Ord symbols
   S:            ['\u00A7', {mathvariant: TexConstant.Variant.NORMAL}],
   aleph:        ['\u2135', {mathvariant: TexConstant.Variant.NORMAL}],
-  hbar:         ['\u210F', {variantForm: true}],
+  hbar:         ['\u0127', {mathvariant: TexConstant.Variant.NORMAL,
+      variantForm: true}],
   imath:        '\u0131',
   jmath:        '\u0237',
   ell:          '\u2113',
